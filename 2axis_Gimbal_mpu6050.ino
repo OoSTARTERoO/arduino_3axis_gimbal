d1e6c208bb798a1c4de99572f6ce540bccdf7401
OoSTARTERoO <dnddl9976@gmail.com>
2016-11-12 17:23:08 +0900

    final code release

---
+++ 2axis_Gimbal_mpu6050.ino
@@ -0,0 +1,121 @@
+#include "I2Cdev.h"
+#include "MPU6050_6Axis_MotionApps20.h"
+#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+  #include "Wire.h"
+#endif
+MPU6050 mpu;
+
+#define LED_PIN 13 // (Arduino is 13, Teensy is 11, Teensy++ is 6)
+bool blinkState = false;
+
+// MPU control/status vars
+bool dmpReady = false; // set true if DMP init was successful
+uint8_t mpuIntStatus;  // holds actual interrupt status byte from MPU
+uint8_t devStatus;   // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;  // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;   // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+// orientation/motion vars
+Quaternion q;      // [w, x, y, z]     quaternion container
+VectorInt16 aa;     // [x, y, z]      accel sensor measurements
+VectorInt16 aaReal;   // [x, y, z]      gravity-free accel sensor measurements
+VectorInt16 aaWorld;  // [x, y, z]      world-frame accel sensor measurements
+VectorFloat gravity;  // [x, y, z]      gravity vector
+float euler[3];     // [psi, theta, phi]  Euler angle container
+float ypr[3];      // [yaw, pitch, roll]  yaw/pitch/roll container and gravity vector
+
+// packet structure for InvenSense teapot demo
+uint8_t teapotPacket[14] = { '$', 0x02, 0,0, 0,0, 0,0, 0,0, 0x00, 0x00, '\r', '\n' };
+
+volatile bool mpuInterrupt = false;   // indicates whether MPU interrupt pin has gone high
+void dmpDataReady() {
+  mpuInterrupt = true;
+}
+
+
+
+
+#include <Servo.h>
+Servo servo1, servo2, servo3;
+int servoPin1=3;
+int servoPin2=12;
+//int servoPin3=11; roll is too unstable
+
+void setup() {
+     servo1.attach(servoPin1);
+     servo2.attach(servoPin2);
+     servo3.attach(servoPin3);
+     servo1.write(0);
+     servo2.write(0);
+//     servo3.write(0); roll is too unstable
+
+  // join I2C bus (I2Cdev library doesn't do this automatically)
+  #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+    Wire.begin();
+    TWBR = 24; // 400kHz I2C clock (200kHz if CPU is 8MHz)
+  #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+    Fastwire::setup(400, true);
+  #endif
+
+  // initialize serial communication
+  // (115200 chosen because it is required for Teapot Demo output, but it's
+  // really up to you depending on your project)
+  Serial.begin(115200);
+  while (!Serial); // wait for Leonardo enumeration, others continue immediately
+  mpu.initialize();
+  devStatus = mpu.dmpInitialize();
+
+  // supply your own gyro offsets here, scaled for min sensitivity
+  mpu.setXGyroOffset(220);
+  mpu.setYGyroOffset(76);
+  mpu.setZGyroOffset(-85);
+  mpu.setZAccelOffset(1788); // 1688 factory default for my test chip
+
+  if (devStatus == 0) {
+    mpu.setDMPEnabled(true);
+    attachInterrupt(0, dmpDataReady, RISING);
+    mpuIntStatus = mpu.getIntStatus();
+    dmpReady = true;
+    packetSize = mpu.dmpGetFIFOPacketSize();
+  }
+  pinMode(LED_PIN, OUTPUT);
+}
+
+
+
+
+void loop() {
+  if (!dmpReady) return;
+  while (!mpuInterrupt && fifoCount < packetSize) {
+  }
+  mpuInterrupt = false;
+  mpuIntStatus = mpu.getIntStatus();
+  fifoCount = mpu.getFIFOCount();
+
+  // overflow check
+  if ((mpuIntStatus & 0x10) || fifoCount == 1024) {
+    mpu.resetFIFO();
+    Serial.println(F("FIFO overflow!"));
+  } else if (mpuIntStatus & 0x02) {
+    while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+    mpu.getFIFOBytes(fifoBuffer, packetSize);
+    fifoCount -= packetSize;
+
+
+    #ifdef OUTPUT_READABLE_YAWPITCHROLL
+      mpu.dmpGetQuaternion(&q, fifoBuffer);
+      mpu.dmpGetGravity(&gravity, &q);
+      mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+            Serial.print("ypr\t");
+            Serial.print(ypr[0] * 180/M_PI);
+            servo1.write(map(ypr[0]*180/M_PI,-90,90,0,180)); 
+            Serial.print("\t");
+            Serial.print(ypr[1] * 180/M_PI);
+            servo2.write(map(ypr[1]*180/M_PI,-90,90,0,180)); 
+            Serial.print("\t");
+            Serial.println(ypr[2] * 180/M_PI);
+            servo3.write(map(ypr[2]*180/M_PI,-90,90,0,180));
+    #endif
+  }
+}
